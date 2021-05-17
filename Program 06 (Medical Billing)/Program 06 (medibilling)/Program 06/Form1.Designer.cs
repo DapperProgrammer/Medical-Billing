b6f65e13c4c8ac2dc55e7d56bb1f58fa27cda1d0
DapperProgrammer <74994236+DapperProgrammer@users.noreply.github.com>
2021-05-17 14:49:59 -0500

    Add files via upload

---
+++ Program 06 (Medical Billing)/Program 06 (medibilling)/Program 06/Form1.Designer.cs
@@ -0,0 +1,274 @@
+﻿namespace Program_06
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtRehab = new System.Windows.Forms.TextBox();
+            this.txtLab = new System.Windows.Forms.TextBox();
+            this.txtSurgical = new System.Windows.Forms.TextBox();
+            this.txtMedication = new System.Windows.Forms.TextBox();
+            this.txtDaysCare = new System.Windows.Forms.TextBox();
+            this.lblStayCharges = new System.Windows.Forms.Label();
+            this.lblMedicalCharges = new System.Windows.Forms.Label();
+            this.lblSum = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Days in care:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(16, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(173, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Medication Charges:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 106);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Surgical Charges:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(16, 156);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(89, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Lab Fees:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(16, 200);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(195, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Physical Rehabilitation:";
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(23, 469);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(106, 61);
+            this.btnCalculate.TabIndex = 5;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(197, 469);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(106, 61);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtRehab
+            // 
+            this.txtRehab.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRehab.Location = new System.Drawing.Point(217, 189);
+            this.txtRehab.Name = "txtRehab";
+            this.txtRehab.Size = new System.Drawing.Size(100, 31);
+            this.txtRehab.TabIndex = 7;
+            // 
+            // txtLab
+            // 
+            this.txtLab.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLab.Location = new System.Drawing.Point(217, 145);
+            this.txtLab.Name = "txtLab";
+            this.txtLab.Size = new System.Drawing.Size(100, 31);
+            this.txtLab.TabIndex = 8;
+            // 
+            // txtSurgical
+            // 
+            this.txtSurgical.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSurgical.Location = new System.Drawing.Point(217, 99);
+            this.txtSurgical.Name = "txtSurgical";
+            this.txtSurgical.Size = new System.Drawing.Size(100, 31);
+            this.txtSurgical.TabIndex = 9;
+            // 
+            // txtMedication
+            // 
+            this.txtMedication.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMedication.Location = new System.Drawing.Point(217, 50);
+            this.txtMedication.Name = "txtMedication";
+            this.txtMedication.Size = new System.Drawing.Size(100, 31);
+            this.txtMedication.TabIndex = 10;
+            // 
+            // txtDaysCare
+            // 
+            this.txtDaysCare.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDaysCare.Location = new System.Drawing.Point(217, 11);
+            this.txtDaysCare.Name = "txtDaysCare";
+            this.txtDaysCare.Size = new System.Drawing.Size(100, 31);
+            this.txtDaysCare.TabIndex = 11;
+            // 
+            // lblStayCharges
+            // 
+            this.lblStayCharges.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblStayCharges.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStayCharges.Location = new System.Drawing.Point(217, 256);
+            this.lblStayCharges.Name = "lblStayCharges";
+            this.lblStayCharges.Size = new System.Drawing.Size(133, 39);
+            this.lblStayCharges.TabIndex = 12;
+            this.lblStayCharges.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblMedicalCharges
+            // 
+            this.lblMedicalCharges.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblMedicalCharges.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMedicalCharges.Location = new System.Drawing.Point(217, 323);
+            this.lblMedicalCharges.Name = "lblMedicalCharges";
+            this.lblMedicalCharges.Size = new System.Drawing.Size(133, 39);
+            this.lblMedicalCharges.TabIndex = 13;
+            this.lblMedicalCharges.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblMedicalCharges.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // lblSum
+            // 
+            this.lblSum.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblSum.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSum.Location = new System.Drawing.Point(217, 377);
+            this.lblSum.Name = "lblSum";
+            this.lblSum.Size = new System.Drawing.Size(133, 39);
+            this.lblSum.TabIndex = 14;
+            this.lblSum.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(16, 275);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(162, 20);
+            this.label9.TabIndex = 15;
+            this.label9.Text = "Total Stay Charges";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(16, 323);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(187, 20);
+            this.label10.TabIndex = 16;
+            this.label10.Text = "Total Medical Charges";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(16, 377);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(90, 20);
+            this.label11.TabIndex = 17;
+            this.label11.Text = "Total Sum";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(360, 533);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.lblSum);
+            this.Controls.Add(this.lblMedicalCharges);
+            this.Controls.Add(this.lblStayCharges);
+            this.Controls.Add(this.txtDaysCare);
+            this.Controls.Add(this.txtMedication);
+            this.Controls.Add(this.txtSurgical);
+            this.Controls.Add(this.txtLab);
+            this.Controls.Add(this.txtRehab);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtRehab;
+        private System.Windows.Forms.TextBox txtLab;
+        private System.Windows.Forms.TextBox txtSurgical;
+        private System.Windows.Forms.TextBox txtMedication;
+        private System.Windows.Forms.TextBox txtDaysCare;
+        private System.Windows.Forms.Label lblStayCharges;
+        private System.Windows.Forms.Label lblMedicalCharges;
+        private System.Windows.Forms.Label lblSum;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ Program 06 (Medical Billing)/Program 06 (medibilling)/Program 06/Form1.cs
@@ -0,0 +1,99 @@
+﻿// Calvin Tracy
+// CS 165 01
+// Program 06
+// 11/27/18
+// Program to accept input for hospital bills and output the total cost.
+
+// We do not use "void" for our custom methods because doing so would mean we could not return any values. 
+// Void means that the method will not give you anything back
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Program_06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public int CalcStayCharges()
+        {
+            int numDays;
+            int stayTotal;
+            int dayCharge = 350;
+
+            // Change "txtDaysCare" from a String to an Int
+            numDays = int.Parse(txtDaysCare.Text);
+            stayTotal = dayCharge * numDays;
+            return stayTotal; // Returns the value of "stayTotal"
+        }
+
+        public double CalcMiscCharges()
+        {
+            double Medication, Surgical, Lab, Rehab, medTotal;
+
+            // Changes all Text boxes from Strings to Doubles
+            Medication = double.Parse(txtMedication.Text);
+            Surgical = double.Parse(txtSurgical.Text);
+            Lab = double.Parse(txtLab.Text);
+            Rehab = double.Parse(txtRehab.Text);
+            medTotal = Medication + Surgical + Lab + Rehab;
+            return medTotal;
+        }
+
+        // Sets "stayTotal" equal to the value of "hospitalCare"
+        // And "mediTotal" equal to the value of "medicalBills"
+        public double CalcTotalCharges(double stayTotal, double mediTotal)
+        {
+            double sumTotal;
+
+            sumTotal = stayTotal + mediTotal;
+            return sumTotal;
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            int hospitalCare;
+            double medicalBills;
+            double sumTotal;
+
+            // hospitalCare is equal to the output of "CalcStayCharges" which is the value of "stayTotal"
+            hospitalCare = CalcStayCharges(); 
+            // Converts the integer value of hospitalCare to a string. 
+            lblStayCharges.Text = hospitalCare.ToString("c");
+
+            // medicalBills is equal to the value in "medTotal"
+            medicalBills = CalcMiscCharges();
+            // Converts the double value of medicalBills to a string.
+            lblMedicalCharges.Text = medicalBills.ToString("c");
+
+            // Passes in the values of "hospitalCare" and "medicalBills."
+            // to the "CalcTotalCharges" function.
+            sumTotal = CalcTotalCharges(hospitalCare, medicalBills);
+            // Changes sumTotal from a double to a String
+            lblSum.Text = sumTotal.ToString("c");
+        }
+
+
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
